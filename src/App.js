7726d773b4990fb458171f9d7afbb8ba40c9b701
GitJohnn <39565975+GitJohnn@users.noreply.github.com>
2022-07-18 22:18:18 -0500

    reverting app change

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Home from "./pages/home/Home";
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
-      {/* <Home /> */}
+      {/* <h1>Hello world!</h1> */}
+      <Home />
     </div>
   );
 }
